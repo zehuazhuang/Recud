e91709b9ea80df00ae2f200d4725aba3b869f989
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2026-04-16 17:24:18 +0800

    1.6.0

--- lib/papilserializngj/proletemobsngchij.dart
+++ lib/papilserializngj/proletemobsngchij.dart
@@ -8,7 +8,7 @@ class Tidalflatydsre {
 
   static String yspineviewse = '75076602';
 
-  static String pmarluggageker = '1.5.0';
+  static String pmarluggageker = '1.6.0';
 
   String dpamementoth = '';
 
